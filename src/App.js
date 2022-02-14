e91003276a7b21dad1123be4d82ebf24fb99b4da
unknown <nicolas.farfan.ch@usach.cl>
2022-02-13 21:38:53 -0300

    Api Preguntas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import ListaPreguntas from "./ListaPreguntas"
+import Pregunta from "./Pregunta"
+import getPreguntas from "./services/getPreguntas"
+import { useState, useEffect } from "react"
 
 function App() {
+  const [question, setQuestion] = useState([])
+  useEffect(() => {
+    getPreguntas().then((promis) => setQuestion(promis.results))
+  }, [])
+  let i = 0
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {question.map((quest) => {
+        i = i + 1
+        console.log(i)
+        return (
+          <Pregunta
+            key={i}
+            p={quest.question}
+            incorrectAnswers={quest.incorrect_answers}
+            correctAnswer={quest.correct_answer}
+          ></Pregunta>
+        )
+      })}
     </div>
-  );
+  )
+  // return (
+  //   <div className="App">
+  //     <h1>My react App</h1>
+  //     <ListaPreguntas></ListaPreguntas>
+  //     {/* <hr></hr> */}
+  //     {/* <Componente id={2} edad={32}></Componente> */}
+  //     {/* <hr></hr> */}
+  //     {/* <Posts></Posts> */}
+  //   </div>
+  // )
 }
 
-export default App;
+export default App
