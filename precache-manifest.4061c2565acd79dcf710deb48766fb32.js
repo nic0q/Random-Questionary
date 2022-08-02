418692e3721f401189b4cbf94b4458bcb1af5a3f
Nícolas <nicolas.farfan.ch@usach.cl>
2022-08-01 20:06:12 -0400

    Updates

---
+++ precache-manifest.4061c2565acd79dcf710deb48766fb32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0a94fffd47de88a23b6",
+    "url": "/Random-Questionary/static/css/main.3f35abf3.chunk.css"
+  },
+  {
+    "revision": "c0a94fffd47de88a23b6",
+    "url": "/Random-Questionary/static/js/main.c0a94fff.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/Random-Questionary/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "f5603992fd358a58ef15",
+    "url": "/Random-Questionary/static/js/2.f5603992.chunk.js"
+  },
+  {
+    "revision": "256e03e430c2e24c1182",
+    "url": "/Random-Questionary/static/js/runtime~main.256e03e4.js"
+  },
+  {
+    "revision": "c6e9e514402384bacc8d5e72e792c415",
+    "url": "/Random-Questionary/index.html"
+  }
+];
